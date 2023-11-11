27f307fa138e9b1b1eeec89bf736a8e5a794c7fa
Alex Ferreira <alex.ferreira1796@gmail.com>
2023-11-11 14:39:47 -0300

    fix: loading component

--- src/components/common/Loading/index.tsx
+++ src/components/common/Loading/index.tsx
@@ -1,6 +1,6 @@
 export const Loading = () => {
   return (
-    <div className="flex items-center justify-center mt-10">
+    <div className="flex items-center justify-center mt-10 mb-10">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
     </div>
   )
